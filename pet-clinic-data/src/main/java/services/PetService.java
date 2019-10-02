9e160fc9f45a3a4080d0d4bebdc79bced7a373cf
Sumit <sumit.ml@gmail.com>
2019-10-03 00:23:54 +0530

    Added Services

---
+++ pet-clinic-data/src/main/java/services/PetService.java
@@ -0,0 +1,12 @@
+package services;
+
+import com.sk.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet>findAll();
+}
---
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -0,0 +1,12 @@
+package services;
+
+import com.sk.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
