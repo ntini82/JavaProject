f97afc3a170cd2badaa3790e9c271042299ef882
Sumit <sumit.ml@gmail.com>
2019-10-07 02:38:33 +0530

    Fixed Broken Links

--- pet-clinic-web/src/main/java/com/sk/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/sk/petclinic/controllers/OwnerController.java
@@ -20,4 +20,9 @@ public class OwnerController {
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping({"/owners/find","/oups"})
+    public String findOwners(){
+        return "notimplemented";
+    }
 }
